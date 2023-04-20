9a3dd33b53ddb715c0c9d8ea259418522a01bbc6
Сергей Золотухин <sergejzolotuhin@Sergejs-MacBook-Air.local>
2023-04-20 13:40:40 +0300

    first commit

---
+++ VKGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKGallery
+//
+//  Created by Сергей Золотухин on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
